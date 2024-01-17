07a1be2f6b3db14a01af18fdd5af3b4628f08ec7
MatiasRamos <mattsk97@gmail.com>
2024-01-17 14:40:46 -0300

    Nav.js styles formatted #cleanCode

--- components/Nav.js
+++ components/Nav.js
@@ -27,33 +27,112 @@ export const navData = [
 // Routing
 import Link from "next/link";
 import { useRouter } from "next/router";
+// Styling
+const navStyling = `
+  flex
+  flex-col
+  items-center
+  gap-y-4
+  fixed
+  h-max
+  bottom-0
+  mt-auto
+  z-50
+  top-0
+  w-full
+  xl:right-[2%]
+  xl:justify-center
+  xl:w-16
+  xl:max-w-md 
+  xl:h-screen`;
+const outerContainerStyling = `
+  flex
+  w-full
+  xl:flex-col
+  items-center
+  justify-between
+  xl:justify-center
+  gap-y-10
+  px-4
+  md:px-40
+  xl:px-0
+  h-[80px]
+  xl:h-max
+  py-8
+  bg-white/10
+  backdrop-blur-sm
+  text-3xl
+  xl:text-xl
+  xl:rounded-full`;
+
+const tooltipTriangleStyling = `
+  border-solid
+  border-l-white
+  border-l-8
+  border-y-transparent
+  border-y-[6px]
+  border-r-0
+  absolute
+  -right-2`;
+const tooltipOuterCtStyling = `
+  absolute
+  pr-14
+  right-0
+  hidden
+  xl:group-hover:flex`;
+const tooltipMiddleCtStyling = `
+  bg-white
+  relative
+  flex
+  text-primary
+  items-center
+  p-[6px]
+  rounded-[3px]`;
+const tooltipInnerCtStyling = `
+  text-[12px]
+  leading-none
+  font-semibold
+  capitalize`;
+
+const linkBaseStyles = `
+  relative
+  flex
+  items-center
+  group
+  hover:text-accent
+  transition-all
+  duration-300`;
+const linkActiveStyles = "text-accent";
 
 const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
+  
 
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+    <nav className={navStyling}>
+      <div className={outerContainerStyling}>
         {navData.map((link, index) => {
+            const linkStyles =
+              link.path === pathname
+                ? `${linkBaseStyles} ${linkActiveStyles}`
+                : linkBaseStyles;
+
           return (
             <Link
               key={index}
               href={link.path}
-              className={`
-                relative flex items-center group hover:text-accent transition-all duration-300
-                ${link.path === pathname && "text-accent"}
-              `}
+              className={linkStyles}
             >
               {/* tooltip */}
-              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex ">
-                <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
-                  <div className="text-[12px] leading-none font-semibold capitalize">
+              <div className={tooltipOuterCtStyling}>
+                <div className={tooltipMiddleCtStyling}>
+                  <div className={tooltipInnerCtStyling}>
                     {link.name}
                   </div>
 
                   {/* triangle */}
-                  <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
+                  <div className={tooltipTriangleStyling}></div>
                 </div>
               </div>
 
