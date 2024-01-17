77b8d61aa7a831962a25b2f4a2f49f269103093f
MatiasRamos <mattsk97@gmail.com>
2024-01-17 18:06:55 -0300

    Home in progress

--- components/Transition.js
+++ components/Transition.js
@@ -25,9 +25,8 @@ const Transition = () => {
         animate='animate'
         exit='exit'
         transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
-      >
-        1
-      </motion.div>
+      ></motion.div>
+
       <motion.div
         className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]'
         variants={transitionVariants}
@@ -35,9 +34,8 @@ const Transition = () => {
         animate='animate'
         exit='exit'
         transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut'}}
-      >
-        1
-      </motion.div>
+      ></motion.div>
+
       <motion.div
         className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
         variants={transitionVariants}
@@ -45,9 +43,8 @@ const Transition = () => {
         animate='animate'
         exit='exit'
         transition={{ delay: 0.6, duration: 0.6, ease: 'easeInOut'}}
-      >
-        1
-      </motion.div>
+      ></motion.div>
+      
     </>
   );
 };
