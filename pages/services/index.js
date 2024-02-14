06d017ce29d152b28eab375839b6f7e762c84144
Matias <matiasramos>
2024-02-13 22:03:20 -0300

    services in progress

--- pages/services/index.js
+++ pages/services/index.js
@@ -12,7 +12,7 @@ const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center ">
       <Circles />
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-56 xl:mt-0">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
@@ -45,7 +45,7 @@ const Services = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="w-full xl:max-w-[65%]"
+            className="w-full xl:max-w-[60%]"
           >
             <ServiceSlider />
           </motion.div>
