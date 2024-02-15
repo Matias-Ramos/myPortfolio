1eeedab55bea0e19d1a81b6e0ca2d5b100fca19d
Matias <matiasramos>
2024-02-15 19:03:51 -0300

    checkpoint

--- pages/work/index.js
+++ pages/work/index.js
@@ -2,7 +2,7 @@
 import WorkSlider from "../../components/WorkSlider";
 import Bulb from '../../components/Bulb';
 import Circles from '../../components/Circles';
-
+import OffCanvas from "../../components/OffCanvas";
 // animation
 import {motion} from 'framer-motion';
 import {fadeIn} from '../../variants';
@@ -11,9 +11,10 @@ import {fadeIn} from '../../variants';
 const Work = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center ">
+      <OffCanvas />
       <Circles />
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-x-8 sm:mt-52">
+        <div className="flex flex-col xl:flex-row gap-x-8 sm:mt-64">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
 
