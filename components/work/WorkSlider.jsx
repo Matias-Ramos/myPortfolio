da4d55a74f5f7e0e22dbe4242ba429dd96f7011d
Matias <matiasramos>
2024-02-28 16:42:30 -0300

    WorkSlider.jsx corrected height on xs and sm viewports

--- components/work/WorkSlider.jsx
+++ components/work/WorkSlider.jsx
@@ -15,7 +15,7 @@ import { fadeIn } from '../../variants';
 // Styles
 const desktopContainerStyle = "hidden md:block w-full xl:max-w-[65%] md:mb-8 z-30"
 const mobileContainerStyle = "md:hidden w-full xl:max-w-[65%] md:mb-28 z-30"
-const swiperStyle = 'h-[280px] xs:auto xs:h-[360px] md:h-[480px] lg:h-[580px] xl:h-[480px]'
+const swiperStyle = 'h-[320px] xs:auto sm:h-[360px] md:h-[480px] lg:h-[580px] xl:h-[480px]'
 const desktopSwiperSlideSt = 'grid md:grid-cols-2 md:grid-rows-2 gap-4 cursor-pointer'
 const mobileSwiperSlideSt = 'grid xs:grid-cols-1 xs:grid-rows-1 cursor-pointer'
 
