b2edbc61ef5a48dc0298f53763ce744ee610c872
Matias <matiasramos>
2024-02-28 21:11:51 -0300

    small margin details on <FaQuoteLeft />

--- components/home/Narrative.jsx
+++ components/home/Narrative.jsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion"
 import { fadeIn } from "../../variants"
 // Styles
-const h1style = "h1 z-10 xl:mb-6 text-danger"
+const h1style = "h1 z-10 xl:mb-5 xl:mt-16 text-danger"
 const paragraphStyle = "w-10/12 xl:w-full my-3 xl:my-0 xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-12 text-lg"
 
 const Narrative = () => (
