ec69d1ca6df06867b7e714fb685b5bfaf72311e5
Matias <matiasramos>
2024-03-06 17:55:31 -0300

    useMediaQuery implementation to render only one Swiper on /work, in progress

--- components/work/WorkSlider.jsx
+++ components/work/WorkSlider.jsx
@@ -9,80 +9,90 @@ import 'swiper/css'
 import 'swiper/css/free-mode'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
-import { Navigation, Pagination  } from 'swiper';
+import { Navigation, Pagination } from 'swiper';
+// Conditional rendering
+import { useMediaQuery } from 'react-responsive'
 // Animation
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 // Styles
-const desktopContainerStyle = "hidden md:block w-full xl:max-w-[75%] md:mb-8"
-const mobileContainerStyle = "md:hidden w-full xl:max-w-[65%] sm:mb-12 md:mb-28"
+const desktopContainerStyle = "w-full xl:max-w-[75%] md:mb-8"
+const mobileContainerStyle = "w-full xl:max-w-[65%] sm:mb-12 md:mb-28"
 const swiperStyle = 'h-[350px] sm:h-[365px] md:h-[480px] lg:h-[580px] xl:h-[500px] xxl:h-[520px]'
 const desktopSwiperSlideSt = 'grid md:grid-cols-2 md:grid-rows-2 gap-4 cursor-pointer px-16 '
 const mobileSwiperSlideSt = 'grid xs:grid-cols-1 xs:grid-rows-1 cursor-pointer md:px-8'
 
 
-const WorkSlider = ({ handleDrawer }) => (
-  <>
-    {/* Desktop */}
-    <motion.div
-      variants={fadeIn('down', 0.6)}
-      initial="hidden"
-      animate="show"
-      exit="hidden"
-      className={desktopContainerStyle}
-    >
-      <Swiper
-        navigation={true}
-        spaceBetween={10}
-        pagination={{ clickable: true }}
-        modules={[Navigation, Pagination]}
-        className={swiperStyle}
-      >
-        {projectList.slides.map((slide, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className={desktopSwiperSlideSt}>
-                {slide.projects.map((project, index) => (
-                  <Card key={index} handleDrawer={handleDrawer} project={project} />
-                ))}
-              </div>
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
-    </motion.div>
+const WorkSlider = ({ handleDrawer }) => {
 
-    {/* Mobile */}
-    <motion.div
-      variants={fadeIn('down', 0.6)}
-      initial="hidden"
-      animate="show"
-      exit="hidden"
-      className={mobileContainerStyle}
-    >
-      <Swiper
-        spaceBetween={10}
-        pagination={{ clickable: true }}
-        modules={[Pagination]}
-        className={swiperStyle}
-      >
-        {projectList.slides.map((slide, index) => {
-          return (
-            <div key={index}>
-              {slide.projects.map( (project) => (
-                <SwiperSlide key={project.link}>
-                  <div className={mobileSwiperSlideSt}>
-                    <Card handleDrawer={handleDrawer} project={project} />
+  const isXl = useMediaQuery({
+    query: '(min-width: 1200px)'
+  })
+
+  return (
+    <>
+      {isXl ?
+        <motion.div
+          variants={fadeIn('down', 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className={desktopContainerStyle}
+        >
+          <Swiper
+            navigation={true}
+            spaceBetween={10}
+            pagination={{ clickable: true }}
+            modules={[Navigation, Pagination]}
+            className={swiperStyle}
+          >
+            {projectList.slides.map((slide, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div className={desktopSwiperSlideSt}>
+                    {slide.projects.map((project, index) => (
+                      <Card key={index} handleDrawer={handleDrawer} project={project} />
+                    ))}
                   </div>
                 </SwiperSlide>
-              ))}
-            </div>
-          )
-        })}
-      </Swiper>
-    </motion.div>
-  </>
-)
+              )
+            })}
+          </Swiper>
+        </motion.div>
+        :
+        <motion.div
+          variants={fadeIn('down', 0.6)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className={mobileContainerStyle}
+        >
+          <Swiper
+            navigation={true}
+            spaceBetween={10}
+            pagination={{ clickable: true }}
+            modules={[Navigation, Pagination]}
+            className={swiperStyle}
+          >
+            {projectList.slides.map((slide, index) => {
+              return (
+                <div key={index}>
+                  {slide.projects.map((project) => (
+                    <SwiperSlide key={project.link}>
+                      <div className={mobileSwiperSlideSt}>
+                        <Card handleDrawer={handleDrawer} project={project} />
+                      </div>
+                    </SwiperSlide>
+                  ))}
+                </div>
+              )
+            })}
+          </Swiper>
+        </motion.div>
+      }
+    </>
+  )
+}
 
 
 export default WorkSlider;
