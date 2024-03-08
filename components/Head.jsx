09529f1bf54432829404589eb91826daec294686
Matias <matiasramos>
2024-03-08 17:02:34 -0300

    description removed from Head

--- components/Head.jsx
+++ components/Head.jsx
@@ -3,7 +3,6 @@ import HeadNextjs from 'next/head';
 const Head = () => (
     <HeadNextjs>
         <title>Matias Ramos</title>
-        <description>Freelance web developer</description>
         <link rel="icon" href="/favicon3.svg"></link>
     </HeadNextjs>
 )
